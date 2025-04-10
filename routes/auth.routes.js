e91c14b8deed34e66698b937b90c9b0eaa5022d6
LiakosV <billiak@hotmail.com>
2025-04-10 13:21:04 +0300

    Google login inserted

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const authController = require('../controllers/auth.controller')
 
-router.post('/login', authController.login)
+router.post('/login', authController.login);
+router.get('/google/callback', authController.googleLogin) // 1
+
 
 module.exports = router;
+
+// https://accounts.google.com/o/oauth2/auth?client_id=89363880805-kjobi9a6lus3dv1f7fbh9urpkcn1oodc.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const { OAuth2Client } = require('google-auth-library')
 
 function generateAccessToken(user) {
   console.log("Auth Service", user)
@@ -26,4 +27,32 @@ function verifyAccessToken(token) {
   }
 }
 
-module.exports = {generateAccessToken, verifyAccessToken}
+async function googleAuth(code) {  //3
+  console.log("Google Login", code);
+  const CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+  const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+  const REDIRECT = process.env.REDIRECT_URI;
+
+  const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT)
+
+  try {
+    // Exchange code for tokens
+    const {tokens} = await oauth2Client.getToken(code)
+    oauth2Client.setCredentials(tokens)
+
+    const ticket = await oauth2Client.verifyIdToken({
+      idToken: tokens.id_token,
+      audience: CLIENT_ID
+    })
+
+    const userInfo = await ticket.getPayload();
+    console.log("Google User", userInfo)
+
+    return{user: userInfo, tokens}
+  } catch (error) {
+    console.log("Error in google authentication", error)
+    return {error: "Failed to authenticate with google"}
+  }
+}
+
+module.exports = {generateAccessToken, verifyAccessToken, googleAuth}
