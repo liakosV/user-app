6d2ca59e57cf784db160df04cb0818647ba75e0c
LiakosV <billiak@hotmail.com>
2025-04-16 20:11:46 +0300

    feat: findLastInsertedUser to user.services created and inserted in test to find the last user

--- __test__/user.test.js
+++ __test__/user.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 
 const authService = require('../services/auth.service')
+const userService = require('../services/user.services');
 
 const app = require('../app');
 // require('dotenv').config();
@@ -41,5 +42,32 @@ describe("Request for /api/users", () => {
     expect(res.statusCode).toBe(200)
     expect(res.body.status).toBeTruthy();
     expect(res.body.data.length).toBeGreaterThan(0);
+  })
+});
+
+describe("Request for /api/users/:username", () => {
+  let token;
+
+  beforeAll(() => {
+    user = {
+      username: "admin",
+      email: "admin@aueb.gr",
+      roles: ["EDITOR", "READER", "ADMIN"]
+    }
+    token = authService.generateAccessToken(user)
+  })
+
+  it("GET returns specific user", async () => {
+    const result = await userService.findLastInsertedUser();
+    console.log("RESULT>>>", result)
+
+    const res = await request(app)
+      .get('/api/users/'+result.username)
+      .set('Authorization', `Bearer ${token}`)
+    
+    expect(res.statusCode).toBe(200)
+    expect(res.body.status).toBeTruthy();
+    expect(res.body.data.username).toBe(result.username);
+    expect(res.body.data.email).toBe(result.email)
   })
 });
--- services/user.services.js
+++ services/user.services.js
@@ -5,9 +5,21 @@ function findAll() {
   return result;
 }
 
-function findOne() {
-  const result = User.findOne();
+function findOne(username) {
+  const result = User.findOne({username: username});
   return result;
 }
 
-module.exports = {findAll, findOne}
+async function findLastInsertedUser() {
+  console.log("Find last inserted user")
+
+  try {
+    const result = await User.find().sort({_id: -1}).limit(1)
+    console.log("Success in finding user", result[0].username)
+    return result[0];
+  } catch (error) {
+    console.log("Problem in finding last inserted user ", error)
+    return false;
+  }
+}
+module.exports = { findAll, findOne, findLastInsertedUser }
