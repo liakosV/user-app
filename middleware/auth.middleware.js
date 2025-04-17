7bd9e2480cc2c5011af0f95465c433c58ce59f79
LiakosV <billiak@hotmail.com>
2025-04-17 16:53:26 +0300

    fix: A console.log has been added in line 10

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -7,7 +7,8 @@ function verifyToken(req, res, next) {
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({status:false, message:"Access denied. No token provided"})
+    console.log("Access Denied")
+    return res.status(401).json({status:false, message:"Access denied. No token provided"}) 
   }
 
   const result = authService.verifyAccessToken(token);
